27170e5a85a8ba54f375aa44df6b963c83008fbb
andrewrgarcia <andrewrgarcia@ufl.edu>
2022-05-25 20:46:55 -0400

    parallel processing pathos

--- lib/module.py
+++ lib/module.py
@@ -7,12 +7,36 @@ Created on Wed May 25 18:21:21 2022
 """
 
 import numpy as np
-
+import pathos.multiprocessing as pathosmp
+# from pathosMP import ProcessingPool as Pool
     
-def slicendice(tensor,L=3):
-    '''slice-n-dice'''
-    return tensor.reshape((*6*[L]))
+
+# def slicendice(tensor,L=3):
+#     '''slice-n-dice
+#     partition higher-order (>= 3D) tensors into dices 
+#     currently beta)
+#     '''
+#     return tensor.reshape((*6*[L]))
 
 
+def single_core(tensor, func, core_idx=1):
 
+    # print('computing on core #{}'.format(core_idx))
     
+    result = func(tensor)
+
+    return result
+
+
+def multi_core(tensor, func, cores = 2):
+    '''split design into parts for each computing core to handle multi-processing tasks'''
+    parts = np.array_split(tensor, cores)
+       
+    var = pathosmp.ProcessingPool().map\
+    (single_core, parts, [func]*cores, list(np.arange(cores)+1))
+                    
+    # results = [ [ *var[i] ] for i in range(cores)]
+    results = [ var[i] for i in range(cores)]
+
+    return results
+
--- tests/test_module.py
+++ tests/test_module.py
@@ -1,17 +1,23 @@
-from lib import module as tes
+from lib import module as scout
 import numpy as np
 
 
-def test_slice():
-    #A = np.arange(1,28).reshape((3,3,3))
-    A = np.arange(9**3).reshape((*3*[9]))
-    AS = tes.slicendice(A,3)
+# def test_slice():
+#     #A = np.arange(1,28).reshape((3,3,3))
+#     A = np.arange(9**3).reshape((*3*[9]))
+#     AS = scout.slicendice(A,3)
     
-    print(AS[(0,0,0)]) #sector 1
-    print(AS[(0,0,1)]) #sector 2
-    # ... etc
+#     print(AS[(0,0,0)]) #sector 1
+#     print(AS[(0,0,1)]) #sector 2
+#     # ... etc
+
+def test_multiproc():
+
+    A = np.random.randint(0,2,(1000,1000))
+    res = scout.multi_core(A, np.count_nonzero, cores = 2)
     
-test_slice()
+    return res
 
-
-
+# test_slice()
+res = test_multiproc()
+print(res)
