86eceb8afa8dd82c5ae025ab68027e4174cd85e1
andrewrgarcia <andrewrgarcia@ufl.edu>
2022-05-28 11:39:35 -0400

    simplified module calls

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='tensorscout',
-      version='1.0',
+      version='1.0.1',
       description='Parallel processing for tensors. A Python library.',
       url='https://github.com/andrewrgarcia/tensorscout',
       author='Andrew Garcia, PhD',
